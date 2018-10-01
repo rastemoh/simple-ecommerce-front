7c0924a1040c31e1a35635862a7d5c614614f6b1
Rastegar <rastemoh@gmail.com>
2018-10-01 16:15:13 +0330

    fixing base api link

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -7,7 +7,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class ApiService {
   constructor(private http: HttpClient) { }
   private token = null;
-  private baseURL = 'http://localhost/dk-commerce/public/index.php/';
+  private baseURL = '/api/';
 
   allProducts() {
     const url = this.baseURL + `products`;
